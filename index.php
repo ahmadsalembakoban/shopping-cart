03e45b161bd6b1c31249cb07717eaf6dc7dbaa4f
ahmadx <ahmad.salem88@gmail.com>
2020-01-22 05:07:32 +0700

    UI build

---
+++ index.php
@@ -0,0 +1,47 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Shopping Cart</title>
+    <!-- bootstrap v4.3.1 -->
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css">
+    <!-- fontawesome v4.7.0 -->
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.8.2/css/all.css">
+</head>
+<body>
+    
+    <div class="container">
+        <div class="row text-center py-5">
+            <div class="col-md-3 col-sm-6 my-3 my-md-0">
+                <form action="index.php" method="post">
+                    <div class="cad shadow">
+                        <div>
+                            <img src="./upload/product1.png" alt="image1" class="img-fluid card-img-top">
+                        </div>
+                        <div class="card-body">
+                            <h5 class="card-title">Product 1</h5>
+                            <h6>
+                                <i class="fas fa-star"></i>
+                            </h6>
+                        </div>
+                    </div>
+                </form>
+            </div>
+            <div class="col-md-3 col-sm-6 my-3 my-md-0"></div>
+            <div class="col-md-3 col-sm-6 my-3 my-md-0"></div>
+            <div class="col-md-3 col-sm-6 my-3 my-md-0"></div>
+        </div>
+    </div>
+
+
+
+
+
+<!-- Bootstrap v4.3 JS -->
+<script src="https://code.jquery.com/jquery-3.3.1.slim.min.js"></script>
+<script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js"></script>
+<script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js"></script>
+</body>
+</html>
